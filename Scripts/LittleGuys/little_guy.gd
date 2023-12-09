5cc9095d0de9ac06dcf418e64c1eeb9f23f41407
Joseph <joseph.aaron.cloud@gmail.com>
2023-12-09 11:38:02 -0500

    initial littleguy movement and spawning prototyped

--- Scripts/LittleGuys/little_guy.gd
+++ Scripts/LittleGuys/little_guy.gd
@@ -2,4 +2,11 @@
 Scripts for individual 'LittleGuy' entities
 """
 
-extends Node2D
+extends CharacterBody2D
+
+@export var speed = 200
+
+func _physics_process(delta):
+	var mouse_position = get_viewport().get_mouse_position()
+	velocity = ( mouse_position - position ).normalized() * speed
+	move_and_slide()
