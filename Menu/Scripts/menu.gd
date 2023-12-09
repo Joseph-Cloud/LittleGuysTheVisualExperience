1b0a9d1dbb6133bd5381acb17a88a87d47498bdc
TapJump <williamda.work@gmail.com>
2023-12-09 12:26:41 -0800

    Hotfix for menu play button and level 1 button in level select

--- Menu/Scripts/menu.gd
+++ Menu/Scripts/menu.gd
@@ -3,8 +3,8 @@ extends Node
 
 #used to call 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Levels/Scene_1.tscn")
-	print("Loading Scene_1.tscn")
+	get_tree().change_scene_to_file("res://Levels/test_level.tscn")
+	print("Loading test_level.tscn")
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Menu/Scenes/Options_menu.tscn")
