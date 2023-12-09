e33a6eceeaeb48e6bad0db26d9c14955e3ce07e1
TapJump <55103183+TapJump@users.noreply.github.com>
2023-12-09 10:41:16 -0800

    Hotfix for menu play button and level 1 button in level select (#6)

--- Menu/Scripts/Level_select.gd
+++ Menu/Scripts/Level_select.gd
@@ -6,14 +6,11 @@ func _ready():
 	pass # Replace with function body.
 
 
-
-
-
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Menu/Scenes/Main_menu.tscn")
 	print("Loading Menu")
 
 
 func _on_level_1_pressed():
-	get_tree().change_scene_to_file("res://Levels/Scene_1.tscn")
-	print("Loading Scene_1.tscn")
+	get_tree().change_scene_to_file("res://Levels/test_level.tscn")
+	print("Loading test_level.tscn")
