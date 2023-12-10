b83a9860731af69ea140898dcabb1b24c87c018f
Ross Terrazas <ross.terrazas97@gmail.com>
2023-12-09 20:17:42 -0600

    Added hazard feature that deletes object when collision happens

--- Scripts/LittleGuys/little_guy.gd
+++ Scripts/LittleGuys/little_guy.gd
@@ -10,3 +10,10 @@ func _physics_process(delta):
 	var mouse_position = get_viewport().get_mouse_position()
 	velocity = ( mouse_position - position ).normalized() * speed
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+
+
+func _on_hazards_body_entered(body):
+	if body.is_in_group("LittleGuy"):
+		print("lava hit")
