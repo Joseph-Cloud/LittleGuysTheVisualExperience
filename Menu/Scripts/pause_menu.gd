65187a3854cd06806ffb2ba65e62b231b64bb22b
TapJump <williamda.work@gmail.com>
2023-12-09 23:14:33 -0800

    Pause menu changes

---
+++ Menu/Scripts/pause_menu.gd
@@ -0,0 +1,22 @@
+extends Node
+#functionality for the buttons in the pause menu
+
+@onready var main = $"../"
+
+
+func _on_home_pressed():
+	Engine.time_scale = 1
+	get_tree().change_scene_to_file("res://Menu/Scenes/Main_menu.tscn")
+	print("Loading Menu")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_options_pressed():
+	print("Need to work on this, will get this going once we get sound working")
+
+
+func _on_resume_pressed():
+	main.pauseMenu()
