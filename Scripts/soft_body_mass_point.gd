927c8100fb51a8d5e76779d280322e1219f67b50
Joseph <joseph.aaron.cloud@gmail.com>
2023-12-10 20:13:48 -0500

    added mouse follow to soft body

--- Scripts/soft_body_mass_point.gd
+++ Scripts/soft_body_mass_point.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 # mass point vars for soft body
 @export var mass = 3
 var spring_force = Vector2.ZERO
+var move_speed = 400
 
 func move():
-	velocity = spring_force / mass
+	var mouse_position = get_viewport().get_mouse_position()
+	var seek_force = (mouse_position - position).normalized() * move_speed
+	velocity = spring_force / mass + seek_force
 	spring_force = Vector2.ZERO
 	move_and_slide()
